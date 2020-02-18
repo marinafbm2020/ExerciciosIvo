74985c008c6939f563ed03aecb0db8b623ce3aec
Treinamento <Treinamento@RD723913.raiadrogasil.com>
2020-02-18 17:51:42 -0300

    Correcao Exercicio02

--- Exercicio02/src/Exercicio02/Triangulo.java
+++ Exercicio02/src/Exercicio02/Triangulo.java
@@ -5,19 +5,17 @@ public class Triangulo {
     private double altura;
     private double area;
 
-    public Triangulo(double base, double altura, double area) {
+    public Triangulo(double base, double altura) {
         this.base = base;
         this.altura = altura;
-        this.area = area;
+
     }
 
-    public Triangulo(double i, int i1) {
-    }
 
 
     public void area(){
        this.area = (this.base*this.altura) /2;
-
+        System.out.println(this.area);
     }
 
     public double getBase() {
